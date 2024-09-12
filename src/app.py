054d8c68981265a5d33476649bacb363c0a5fdb8
scott-22 <scotthao65@gmail.com>
2024-09-12 00:40:39 -0400

    Open Foxglove visualization stuff from link

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
+from flask_rangerequest import RangeRequest
+from flask_cors import cross_origin
+from datetime import datetime
+from os import path
 from util import *
 
 app = Flask(__name__)
 
+
+FILE = "static/rosbag/all_sensors_for_so_verytcut_2023-01-07-VEH8-LVMS-RUN-6_0.mcap"
+SIZE = path.getsize(FILE)
+with open(FILE, 'rb') as f:
+    ETAG = RangeRequest.make_etag(f)
+LAST_MODIFIED=datetime.utcnow()
 
 @app.route("/")
 def index():
@@ -51,6 +61,16 @@ def filter_metadata():
     runs = list(filter_by_metadata(combine_metadata_filters(filters)))
 
     return {"runs": runs}
+
+@app.route("/rosbag.mcap", methods=["GET", "POST"])
+@cross_origin(origins="https://app.foxglove.dev", expose_headers=["ETag", "Content-Type", "Accept-Ranges", "Content-Length"])
+def return_rosbag():
+    return RangeRequest(
+        open(FILE, "rb"),
+        etag=ETAG,
+        size=SIZE,
+        last_modified=LAST_MODIFIED,
+    ).make_response()
 
 
 if __name__ == "__main__":
